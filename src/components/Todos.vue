59be082b5cc944a8adf56afc862c84272790b0e3
lytves <22356770+lytves@users.noreply.github.com>
2019-02-05 16:08:08 +0100

    Todolist app created

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+        <div v-bind:key="todo.id" v-for="todo in todos">
+
+            <TodoItem v-bind:todo="todo"></TodoItem>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import TodoItem from "@/components/TodoItem.vue";
+
+    export default {
+        name: "Todos",
+        components: {
+            TodoItem
+        },
+        props:["todos"]
+    }
+</script>
+
+<style scoped>
+
+</style>
