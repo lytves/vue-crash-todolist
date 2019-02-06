bfeb41b3a386e42a3f7ec37a6284a052c18b512e
lytves <22356770+lytves@users.noreply.github.com>
2019-02-06 15:40:17 +0100

    implemented todolist header and method to add new todo items

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
     <div id="app">
+        <Header></Header>
+        <AddTodo v-on:add-todo="addTodo"></AddTodo>
         <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"></Todos>
     </div>
 </template>
 
 <script>
+    import Header from "@/components/layout/Header.vue"
     import Todos from "@/components/Todos.vue"
+    import AddTodo from "@/components/AddTodo.vue"
 
     export default {
         name: 'app',
         components: {
-            Todos
+            Header,
+            Todos,
+            AddTodo
         },
         data() {
             return {
@@ -36,6 +42,9 @@
         methods: {
             deleteTodo(id) {
                 this.todos = this.todos.filter(todo => todo.id !== id);
+            },
+            addTodo(newTodo) {
+                this.todos = [...this.todos, newTodo];
             }
         }
     }
